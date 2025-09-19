1d7bd5db152d4eff5d2268280616c3e688383027
Temesgen Keife <64307949+TemuSoft@users.noreply.github.com>
2025-09-19 22:03:11 +0300

    Game data initialize

--- app/src/main/java/com/ChickenArsenal/CA2508/GameActivity.java
+++ app/src/main/java/com/ChickenArsenal/CA2508/GameActivity.java
@@ -1,24 +1,232 @@
 package com.ChickenArsenal.CA2508;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class GameActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class GameActivity extends AppCompatActivity implements View.OnTouchListener {
+    private LinearLayout layout_canvas, layout_ui, layout_blur, layout_dialog;
+
+    private ImageView home_dialog, chicken;
+    private Button status, next_again;
+    private TextView egg_left;
+
+    private ImageView home;
+    private ProgressBar progressBar;
+    private TextView level;
+
+    private LayoutInflater inflate;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private boolean isMute, soundMute;
+    private Intent intent;
+    private String lang;
+    private AlertDialog.Builder builder;
+    private Random random;
+    private Handler handler;
+    private GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setStatusBarColor(getResources().getColor(R.color.trans));
+        sharedPreferences = getSharedPreferences("hienArsen12o8", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        isMute = sharedPreferences.getBoolean("isMute", false);
+        soundMute = sharedPreferences.getBoolean("soundMute", false);
+        lang = sharedPreferences.getString("lang", "");
+
         setContentView(R.layout.activity_game);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        builder = new AlertDialog.Builder(this);
+        random = new Random();
+        handler = new Handler();
+
+        layout_canvas = findViewById(R.id.layout_canvas);
+        inflate = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        layout_ui = findViewById(R.id.layout_ui);
+        layout_blur = findViewById(R.id.layout_blur);
+        layout_dialog = findViewById(R.id.layout_dialog);
+
+        home_dialog = findViewById(R.id.home_dialog);
+        chicken = findViewById(R.id.chicken);
+        status = findViewById(R.id.status);
+        next_again = findViewById(R.id.next_again);
+        egg_left = findViewById(R.id.egg_left);
+
+        home = findViewById(R.id.home);
+        progressBar = findViewById(R.id.progressBar);
+        level = findViewById(R.id.level);
+
+        layout_canvas.removeAllViews();
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+
+        int w = point.x;
+        int h = point.y;
+        gameView = new GameView(this, w, h, getResources(), 0);
+        gameView.setLayoutParams(new LinearLayout.LayoutParams(w, h));
+        layout_canvas.addView(gameView);
+
+        layout_canvas.setOnTouchListener(this);
+        reloading_UI();
+
+        layout_blur.setVisibility(GONE);
+        layout_dialog.setVisibility(GONE);
+        layout_ui.setVisibility(VISIBLE);
+        layout_canvas.setVisibility(VISIBLE);
+
+        level.setText(getResources().getString(R.string.level) + " " + gameView.playLevel);
+
+        home.setOnClickListener(View -> {
+            Player.button(soundMute);
+
+            finish();
         });
+    }
+
+    private void reloading_UI() {
+        Runnable r = new Runnable() {
+            public void run() {
+                if (gameView.isPlaying) {
+                    if (gameView.game_over && gameView.game_over_time + gameView.duration < System.currentTimeMillis())
+                        game_over();
+
+                    if (gameView.game_won && gameView.game_won_time + gameView.duration < System.currentTimeMillis())
+                        game_won();
+
+                    if (!gameView.game_over && !gameView.game_won) gameView.update();
+
+                    progressBar.setProgress(gameView.remain_eggs * 100 / gameView.total_eggs);
+
+                    reloading_UI();
+                }
+            }
+        };
+        handler.postDelayed(r, 20);
+    }
+
+    private void game_over() {
+        gameView.isPlaying = false;
+
+        layout_ui.setVisibility(GONE);
+        layout_blur.setVisibility(VISIBLE);
+        layout_dialog.setVisibility(VISIBLE);
+
+        status.setText(R.string.level_faild);
+        egg_left.setText(R.string.eggs_left + " " + gameView.remain_eggs);
+        chicken.setImageResource(R.drawable.chicken_stand);
+
+        next_again.setOnClickListener(View -> {
+            Player.button(soundMute);
+
+            intent = new Intent(GameActivity.this, GameActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+    }
+
+    private void game_won() {
+        gameView.isPlaying = false;
+
+        layout_ui.setVisibility(GONE);
+        layout_blur.setVisibility(VISIBLE);
+        layout_dialog.setVisibility(VISIBLE);
+
+        status.setText(R.string.level_completed);
+        egg_left.setText(R.string.eggs_left + " " + gameView.remain_eggs);
+        chicken.setImageResource(R.drawable.chicken_fly);
+
+        next_again.setOnClickListener(View -> {
+            Player.button(soundMute);
+            gameView.playLevel++;
+            if (gameView.playLevel > gameView.lastLevelActive)
+                gameView.lastLevelActive = gameView.playLevel;
+
+            editor.putInt("lastLevelActive", gameView.lastLevelActive);
+            editor.putInt("playLevel", gameView.playLevel);
+            editor.apply();
+
+            intent = new Intent(GameActivity.this, GameActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.isPlaying = false;
+        if (!isMute) Player.all_screens.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.isPlaying = true;
+        isMute = sharedPreferences.getBoolean("isMute", false);
+        if (!isMute) Player.all_screens.start();
+    }
+
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+        return;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                processActionDown(x, y);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                processActionMove(x, y);
+                break;
+            case MotionEvent.ACTION_UP:
+                processActionUp(x, y);
+                break;
+        }
+        return true;
+    }
+
+    private void processActionDown(int x, int y) {
+
+    }
+
+    private void processActionUp(int xp, int yp) {
+        Rect clicked = new Rect(xp, yp, xp, yp);
+
+    }
+
+    private void processActionMove(int x, int y) {
+
     }
 }
---
+++ app/src/main/java/com/ChickenArsenal/CA2508/GameView.java
@@ -0,0 +1,115 @@
+package com.ChickenArsenal.CA2508;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GameView extends View {
+    private SharedPreferences sharedPreferences;
+    private int screenX, screenY;
+    private Resources resources;
+    private Random random;
+    boolean isPlaying = true, game_over, game_won;
+    int duration = 1000;
+    long game_over_time, game_won_time = 0;
+
+    int score;
+    private int xSpeed, ySpeed;
+    private Context context;
+    int lastLevelActive, playLevel;
+    int total_eggs, remain_eggs;
+
+    int active_egg;
+    Bitmap cloud, sun, ground, egg, container, basket, danger_0, danger_1, danger_2;
+    int c_w, c_h, s_w_h, g_w, g_h, e_w, e_h, b_w, b_h, con_w, con_h;
+    int dan_w_0, dan_h_0, dan_w_1, dan_h_1, dan_w_2, dan_h_2;
+
+    ArrayList<ArrayList<Integer>> cloud_data = new ArrayList<>();
+
+    public GameView(Context mContext, int scX, int scY, Resources res, int level_amount) {
+        super(mContext);
+        screenX = scX;
+        screenY = scY;
+        resources = res;
+        context = mContext;
+        random = new Random();
+
+        sharedPreferences = context.getSharedPreferences("hienArsen12o8", context.MODE_PRIVATE);
+        lastLevelActive = sharedPreferences.getInt("lastLevelActive", 1);
+        playLevel = sharedPreferences.getInt("playLevel", 1);
+        active_egg = sharedPreferences.getInt("active_egg", 0);
+
+        total_eggs = 5;
+        remain_eggs = 2;
+
+        int e = context.getResources().getIdentifier("egg_" + active_egg, "drawable", context.getPackageName());
+        cloud = BitmapFactory.decodeResource(res, R.drawable.cloud);
+        sun = BitmapFactory.decodeResource(res, R.drawable.sun);
+        ground = BitmapFactory.decodeResource(res, R.drawable.ground);
+        egg = BitmapFactory.decodeResource(res, e);
+        container = BitmapFactory.decodeResource(res, R.drawable.container);
+        basket = BitmapFactory.decodeResource(res, R.drawable.basket);
+
+        danger_0 = BitmapFactory.decodeResource(res, R.drawable.danger_0);
+        danger_1 = BitmapFactory.decodeResource(res, R.drawable.danger_1);
+        danger_2 = BitmapFactory.decodeResource(res, R.drawable.danger_2);
+
+        c_w = cloud.getWidth();
+        c_h = cloud.getHeight();
+        s_w_h = sun.getWidth();
+        g_w = ground.getWidth();
+        g_h = ground.getHeight();
+        e_w = egg.getWidth();
+        e_h = egg.getHeight();
+        con_w = container.getWidth();
+        con_h = container.getHeight();
+        b_w = basket.getWidth();
+        b_h = basket.getHeight();
+
+        dan_w_0 = danger_0.getWidth();
+        dan_h_0 = danger_0.getHeight();
+        dan_w_1 = danger_1.getWidth();
+        dan_h_1 = danger_1.getHeight();
+        dan_w_2 = danger_2.getWidth();
+        dan_h_2 = danger_2.getHeight();
+
+        cloud = Bitmap.createScaledBitmap(cloud, c_w, c_h, false);
+        sun = Bitmap.createScaledBitmap(sun, s_w_h, s_w_h, false);
+        ground = Bitmap.createScaledBitmap(ground, g_w, g_h, false);
+        egg = Bitmap.createScaledBitmap(egg, e_w, e_h, false);
+        container = Bitmap.createScaledBitmap(container, con_w, con_h, false);
+        basket = Bitmap.createScaledBitmap(basket, b_w, b_h, false);
+        danger_0 = Bitmap.createScaledBitmap(danger_0, dan_w_0, dan_w_0, false);
+        danger_1 = Bitmap.createScaledBitmap(danger_0, dan_w_1, dan_w_1, false);
+        danger_2 = Bitmap.createScaledBitmap(danger_0, dan_w_2, dan_w_2, false);
+
+        setSpeed();
+    }
+
+    public void onDraw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawColor(Color.TRANSPARENT);
+
+
+    }
+
+    private void setSpeed() {
+        xSpeed = screenX / 80;
+        ySpeed = screenY / 80;
+    }
+
+    public void update() {
+
+        invalidate();
+    }
+}
